8c81fe53a092ed4e913410c7d337deba8ca7f19d
VigneshChetti <vigneshchetty8973@gmail.com>
2023-01-02 19:50:22 +0530

    commiting

---
+++ spring-security-jpa/src/main/java/com/atmecs/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,17 @@
+package com.atmecs.springsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.atmecs.springsecurityjpa.repository.UserRepository;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
